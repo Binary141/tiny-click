664b8f20158e769bc5dc29324e52a55a7fb636f9
Brendan Davidson <binary141@gmail.com>
2024-07-03 11:35:13 -0600

    Add in dropdown select for the http scheme

--- tiny-click/src/components/AddURLs.vue
+++ tiny-click/src/components/AddURLs.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="about">
     <h3 style="text-align: center">Add a URL</h3>
+    <form>
+        <p>
+            URL: 
+            <label for="scheme"></label>
+            <select id="scheme" name="scheme">
+                <option value="http">http://</option>
+                <option value="https">https://</option>
+            </select>
+            <input style='margin:7px' type='text' id='newURL'>
+        </p>
+    </form>
+    <button @click="buttonClick">Submit</button>
   </div>
 
-<button @click="buttonClick">Submit</button>
 </template>
 
 <script>
+    const serverURL = import.meta.env.VITE_SERVER_URL
     export default {
-  methods: {
-    buttonClick(event) {
-      console.log("button was clicked");
+        methods: {
+            async buttonClick(event) {
+                var url = document.getElementById('newURL').value
+                var scheme = document.getElementById('scheme').value
+
+                var resp = await(fetch(`${serverURL}/insert?redirectURL=${scheme}://${url}`))
+                console.log(resp);
+                console.log("button was clicked")
+            }
+        }
     }
-  }
-}
 </script>
